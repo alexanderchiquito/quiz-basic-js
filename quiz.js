88d3fd1744ca3caedf828eda71d3488af61433a9
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-07-10 18:48:55 -0500

    fix component

---
+++ quiz.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        id: 1,
+        question: "what's the capital of France?",
+        answers: [
+            {answer: "Roma", isCorrect: false},
+            {answer: "Paris", isCorrect: true},
+            {answer: "Toronto", isCorrect: false},
+            {answer: "Caracas", isCorrect: false}
+        ],
+    },
+    {
+        id: 2,
+        question: "In what year was America discovered by Christopher Columbus?",
+        answers: [
+            {answer: "1492 ", isCorrect: true},
+            {answer: "1789", isCorrect: false},
+            {answer: "1844", isCorrect: false},
+            {answer: "1605", isCorrect: false}
+        ],
+    },
+    {
+        id: 3,
+        question: "What is the longest river in the world?",
+        answers: [
+            {answer: "Nilo", isCorrect: true},
+            {answer: "Amazonas", isCorrect: false},
+            {answer: "Mississippi", isCorrect: false},
+            {answer: "Yangtze", isCorrect: false}
+        ],
+    },
+    {
+        id: 4,
+        question: "What is the chemical symbol for gold?",
+        answers: [
+            {answer: "Ag", isCorrect: false},
+            {answer: "Hg", isCorrect: false},
+            {answer: "Au ", isCorrect: true},
+            {answer: "Fe", isCorrect: false}
+        ],
+    },
+    {
+        id: 5,
+        question: "Who wrote the famous novel 'One Hundred Years of Solitude'",
+        answers: [
+            {answer: "Pablo Neruda", isCorrect: false},
+            {answer: "Cristiano Ronaldo", isCorrect: true},
+            {answer: "Mario Vargas Llosa", isCorrect: false},
+            {answer: " Gabriel García Márquez", isCorrect: false}
+        ],
+    },
+]
